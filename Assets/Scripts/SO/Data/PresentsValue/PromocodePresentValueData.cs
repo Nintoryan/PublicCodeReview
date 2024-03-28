b836d6b35019529a022bda90ddacf13d8cb068ad
Nintoryan <thehappystudio4@gmail.com>
2024-03-28 20:19:49 +0300

    Initial commit

---
+++ Assets/Scripts/SO/Data/PresentsValue/PromocodePresentValueData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace SO.Data.Presents
+{
+    [CreateAssetMenu(fileName = "PresentValue_Promocode_", menuName = "SO/Data/Presenets/Value/PresentValue_Promocode")]
+    public class PromocodePresentValueData: PresentValueData
+    { 
+        public int PromocodeID;
+        [TextArea(5,10)] public string Description;
+        public override bool Equivalent(PresentValueData other)
+        {
+            if (other != null && other is PromocodePresentValueData promocodePresentValueData)
+            {
+                return PromocodeID == promocodePresentValueData.PromocodeID;
+            }
+
+            return false;
+        }
+    }
+}
